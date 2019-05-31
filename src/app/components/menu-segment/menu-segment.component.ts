a5d5448b497397ed84827d16134f2c5c7b28ab1a
gh0stw3b <salvador.martinez@alumno.uttehuacan.edu.mx>
2019-05-30 23:20:05 -0500

    menu-segment

--- src/app/components/menu-segment/menu-segment.component.ts
+++ src/app/components/menu-segment/menu-segment.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './menu-segment.component.html',
   styleUrls: ['./menu-segment.component.scss'],
 })
+
 export class MenuSegmentComponent implements OnInit {
 
   @ViewChild(IonSegment) s: IonSegment;
