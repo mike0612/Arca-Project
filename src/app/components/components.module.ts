b4edbcfa4d6c2ec5128562440ea3c87c37440525
gh0stw3b <salvador.martinez@alumno.uttehuacan.edu.mx>
2019-06-03 17:49:03 -0500

    badge guardar tip

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { MenuSegmentComponent } from './menu-segment/menu-segment.component';
-import { TipComponent } from './tip/tip.component';
-
 
 @NgModule({
   declarations: [
