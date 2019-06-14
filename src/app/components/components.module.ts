2f9ecfaa6198d2e3d61ed0bae7a2583c3a9ffb09
mike0612 <mike.galv0612@gmail.com>
2019-06-14 01:06:39 -0500

    Pruebas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,44 +5,24 @@ import { MenuSegmentComponent } from './menu-segment/menu-segment.component';
 import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { customTranslateLoader } from './translate';
-import { AdoptaPage } from '../pages/adopta/adopta.page';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'menu-segment',
-    component: AdoptaPage,
-    children: [
-      {
-        path: 'adopta',
-        loadChildren: './pages/adopta/adopta.module#AdoptaPageModule'
-      },
-     
-      
-    ]
-  },
-  {
-    path: '',
-    redirectTo: 'menu-segment/adopta',
-    pathMatch: 'full'
-  }
-];
+
 
 
 @NgModule({
   declarations: [
     MenuSegmentComponent,    
-    [AdoptaPage]
+   
   ],
   exports: [
     MenuSegmentComponent,    
-
+  
   
   ],
   imports: [
     CommonModule, 
     IonicModule,   
-    RouterModule.forChild(routes),
+   
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
@@ -52,7 +32,7 @@ const routes: Routes = [
       }
       
     }),
-    
+ 
   ],
 })
 
